9c020bb1a4bbf5a6cc0d3b9c40979d4eb731a74d
Ivan Storck <ivanoats@gmail.com>
2013-06-17 13:59:31 -0700

    basic perf test fail

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tblog2::Application.config.secret_token = '502112d958ba6894097b692869800406184ebaa3d7b3c08d39ed9ad0ebf0c3498ca23ca96b31607b540e9d074574d610e6cb0865b33882f748aaff6d3f866426'
